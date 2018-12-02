7c0d8aaa4ab6aab20cb15cf7b44856cfe83a5432
avrellmenezes <43628147+avrellmenezes@users.noreply.github.com>
2018-12-02 13:49:06 +0800

    Basket UI

---
+++ MyShop/MyShop.WebUI/Controllers/BasketController.cs
@@ -0,0 +1,41 @@
+﻿using MyShop.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class BasketController : Controller
+    {
+        IBasketService BasketService;
+        public BasketController(IBasketService BasketService)
+        {
+            this.BasketService = BasketService;
+        }
+        // GET: Basket
+        public ActionResult Index()
+        {
+            var model = BasketService.GetBasketItems(this.HttpContext);
+            return View(model);
+        }
+        public ActionResult AddToBasket(string productId)
+        {
+            BasketService.AddToBasket(this.HttpContext, productId);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult RemoveFromBasket(string Id)
+        {
+            BasketService.RemoveFromBasket(this.HttpContext, Id);
+            return RedirectToAction("Index");
+        }
+
+        public PartialViewResult BasketSummary()
+        {
+            var basketSummary = BasketService.GetBasketSummary(this.HttpContext);
+            return PartialView(basketSummary);
+        }
+    }
+}
